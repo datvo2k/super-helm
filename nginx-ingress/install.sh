a7bd80915edbbe2985dce80ae74bc9f309c14e2b
Brian Vo <datvo2k@gmail.com>
2025-01-06 00:32:36 +0700

    add nginx-ingress installation script

---
+++ nginx-ingress/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+kubectl create namespace nginx-ingress
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install nginx-ingress ingress-nginx/ingress-nginx --set controller.publishService.enabled=true --namespace nginx-ingress
